fff0866d13ab4f659f0c95b1c0f34acdf4cdce77
LaibaMunir16 <bsef23m021@pucit.edu.pk>
2025-11-13 08:30:14 +0500

    Add docstrings to reduce Pylint warnings

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+"""This module demonstrates a Python app for Jenkins CI/CD."""
+
 def main():
+    """Prints a message for Jenkins CI/CD demo."""
     print("Hello Jenkins, code updated automatically!")
 
 if __name__ == "__main__":
